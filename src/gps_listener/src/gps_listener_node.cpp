c51c5aaca58f91ecddf4ff3dafd8e4ae2cda916f
seu-lc <220180276@seu.edu.cn>
2020-07-19 12:13:21 +0800

    - add listener

--- src/gps_listener/src/gps_listener_node.cpp
+++ src/gps_listener/src/gps_listener_node.cpp
@@ -1,4 +1,4 @@
-//ROS头文件
+
 #include <ros/ros.h>
 #include "nav_msgs/Odometry.h"
 
@@ -13,7 +13,7 @@ void gpsCallback(const nav_msgs::Odometry::ConstPtr& msg)
     ROS_INFO("longitude : .2f%",ll2utm_msg.pose.covariance[0]);
     ROS_INFO("latitude : .2f%",ll2utm_msg.pose.covariance[1]);
     ROS_INFO("yaw (rad) : .2f",ll2utm_msg.pose.covariance[2]);
-    ROS_INFO("speed vx (m/s): .2f%",ll2utm_msg.pose.covariance[0]); 
+    ROS_INFO("speed vx (m/s): .2f%",ll2utm_msg.twist.twist.linear.x); 
 }
 
 int main(int argc, char **argv)
@@ -21,7 +21,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "gps_listener_node");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("/ll2utm_odom", 1, gpsCallback);
-  //ros::spin()用于调用所有可触发的回调函数。将进入循环，不会返回，类似于在循环里反复调用ros::spinOnce()。
   ros::spin(); 
   return 0;
 }
